35a24f82432a7af4a1d777160a68fc63720b76df
subhanshu jha <subhanshujha001@gmail.com>
2024-09-22 13:22:49 +0530

    links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import Link from "next/link";
+import { FaTwitter } from "react-icons/fa";
 import {FaGithub} from "react-icons/fa"
+import { FaLinkedin } from "react-icons/fa";
 export default function Home() {
   return (
     <div className="min-w-full min-h-[100vh] items-center flex-row flex ">
 <div className="flex flex-col justify-center min-w-[40vw] text-white min-h-[100vh] items-center bg-[#000048]">
-<h1 className="text-3xl font-bold p-6">Algorithm Visualier</h1>
+<h1 className="text-3xl font-bold p-6">Algorithm Visualizer</h1>
 <p>Understanding popular algoritms in a visual format </p>
 </div>
 <div className="flex flex-col min-w-[50vw] min-h-[90vh] items-center justify-between">
@@ -19,8 +21,14 @@ export default function Home() {
 <p>visual demonstartion of varoius path Sorting algoritms with beautiful animations </p>
 <p className="font-bold">work in progress</p>
 </div>
-<Link href="https://github.com/subhanahujha007" className="mr-[300px]"><FaGithub size={40}/>
+<div className="flex flex-row justify-around max-w-[200px] gap-2 mr-[500px] ">
+<Link href="https://github.com/subhanahujha007" ><FaGithub size={40}/>
 </Link>
+<Link href="https://x.com/035Subhans67914" ><FaTwitter size={40}/>
+</Link>
+<Link href="https://www.linkedin.com/in/subhanshu-jha-1429a1195/" ><FaLinkedin size={40}/>
+</Link>
+</div>
 </div>
   </div>
   );
